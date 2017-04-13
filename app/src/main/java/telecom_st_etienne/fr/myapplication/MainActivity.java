08509cbdbda4b78ae24689cc4a552dc4077bef35
Jérémy Falck <Jrmy Falck>
2017-04-13 18:39:03 +0200

    Initial commit

---
+++ app/src/main/java/telecom_st_etienne/fr/myapplication/MainActivity.java
@@ -0,0 +1,26 @@
+package telecom_st_etienne.fr.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("Localisation Axians");
+
+        Button button6 = (Button) findViewById(R.id.connect);
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
