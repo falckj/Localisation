bb7a4bbeaecd63419e4424fef9bd7da9733a6c0b
Jérémy Falck <falckj@users.noreply.github.com>
2017-06-13 10:45:47 +0200

    Create SingleUser.java

--- app/src/main/java/telecom_st_etienne/fr/myapplication/UserTracking/SingleUser.java
+++ app/src/main/java/telecom_st_etienne/fr/myapplication/UserTracking/SingleUser.java
@@ -14,12 +14,21 @@ import telecom_st_etienne.fr.myapplication.UsefulMethods.BaseClass;
 import telecom_st_etienne.fr.myapplication.UsefulMethods.ConfigReader;
 import telecom_st_etienne.fr.myapplication.UsefulMethods.Point;
 
+/*
+* Cette classe permet d'obtenir les informations liées à un utilisateur virtuel sur le site de la sandbox.
+* Cet utilisateur est authentifié à partir des informations contenues dans le fichier config4.ini
+*/
+
 public class SingleUser extends BaseClass {
 
 	public static void main(String[] args) throws InterruptedException, IOException {
 
 		try {
-
+			/*
+			* C'est ici que nous n'arrivons pas à ouvrir le fichier :
+			* dans l'arborescence Android, les "\" sont remplacés par des points
+			* Même si on spécifie le bon chemin, Android ne trouve pas le fichier.
+			*/
 			config = ConfigReader.readConfig("R.raw.config3");
 		} catch (Exception e) {
 			e.printStackTrace();
